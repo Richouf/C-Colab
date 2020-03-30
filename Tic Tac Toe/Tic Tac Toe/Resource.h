1a916a16640121c19d981beeefe3003fb5eaa1b0
Richouf <62738691+Richouf@users.noreply.github.com>
2020-03-29 20:38:46 -0500

    Revert "."
    This reverts commit 928d53d67879de63988bf80f926835f30b39c84f.

--- Tic Tac Toe/Tic Tac Toe/Resource.h
+++ Tic Tac Toe/Tic Tac Toe/Resource.h
@@ -13,7 +13,6 @@
 #define IDI_SMALL				108
 #define IDC_TICTACTOE			109
 #define IDC_MYICON				2
-#define ID_HELP_PRINT			32771
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
 #endif
@@ -24,7 +23,7 @@
 
 #define _APS_NO_MFC					130
 #define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32772
+#define _APS_NEXT_COMMAND_VALUE		32771
 #define _APS_NEXT_CONTROL_VALUE		1000
 #define _APS_NEXT_SYMED_VALUE		110
 #endif
